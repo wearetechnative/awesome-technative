3367876d7466098170469063549a3cb9b62ce342
Pim Snel <post@pimsnel.com>
2022-09-01 13:00:00 +0200

    impr casing

--- RUNME.sh
+++ RUNME.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 . ./.runme.sh
 
-make_command "make_toc" "Generate new Table Of Contents"
+make_command "make_toc" "generate new table of contents"
 make_toc(){
   cat readme.md | sed -e 's/##/#/g' | ./gh-md-toc - | tail -n +3
 }
